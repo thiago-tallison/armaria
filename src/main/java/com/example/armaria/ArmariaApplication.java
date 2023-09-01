17137d0e1be35894a74cfe3cfd7e9d0f880a08ab
Thiago Tallison <thiagotallison@gmail.com>
2023-09-01 09:48:13 -0300

    initial commit

---
+++ src/main/java/com/example/armaria/ArmariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.armaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/armaria/ArmariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.armaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
