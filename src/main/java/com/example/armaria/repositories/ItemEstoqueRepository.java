1e79f8a76386de5e72482f2a990744f8a98432a1
Thiago Tallison <thiagotallison@gmail.com>
2023-09-04 19:23:16 -0300

    refactor: busca de ItemEstoque por Equipamento

--- src/main/java/com/example/armaria/repositories/ItemEstoqueRepository.java
+++ src/main/java/com/example/armaria/repositories/ItemEstoqueRepository.java
@@ -1,9 +1,14 @@
 package com.example.armaria.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.armaria.entities.Equipamento;
 import com.example.armaria.entities.ItemEstoque;
 
 public interface ItemEstoqueRepository extends JpaRepository<ItemEstoque, Long> {
+
+  Optional<ItemEstoque> findByEquipamento(Equipamento equipamento);
 
 }
