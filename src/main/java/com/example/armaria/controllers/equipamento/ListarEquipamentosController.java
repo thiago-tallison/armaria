8fec9476cbc21c1934611dc0d14cc17c1d220aa5
Thiago Tallison <thiagotallison@gmail.com>
2023-09-04 20:00:32 -0300

    feat: caso de uso de listar equipamentos

---
+++ src/main/java/com/example/armaria/controllers/equipamento/ListarEquipamentosController.java
@@ -0,0 +1,33 @@
+package com.example.armaria.controllers.equipamento;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.armaria.use_cases.equipamento.ListarEquipamentoResponseDTO;
+import com.example.armaria.use_cases.equipamento.ListarEquipamentosRequestDTO;
+import com.example.armaria.use_cases.equipamento.ListarEquipamentosUseCase;
+
+@RestController
+@RequestMapping("/api/equipamento")
+public class ListarEquipamentosController {
+  private final ListarEquipamentosUseCase listarEquipamentosUseCase;
+
+  @Autowired
+  public ListarEquipamentosController(ListarEquipamentosUseCase listarEquipamentosUseCase) {
+    this.listarEquipamentosUseCase = listarEquipamentosUseCase;
+  }
+
+  @GetMapping()
+  public ResponseEntity<ListarEquipamentoResponseDTO> handle(@RequestParam Integer pagina,
+      @RequestParam Integer itensPorPagina) {
+    ListarEquipamentosRequestDTO dto = new ListarEquipamentosRequestDTO(itensPorPagina, pagina);
+
+    ListarEquipamentoResponseDTO response = listarEquipamentosUseCase.execute(dto);
+
+    return ResponseEntity.ok().body(response);
+  }
+}
