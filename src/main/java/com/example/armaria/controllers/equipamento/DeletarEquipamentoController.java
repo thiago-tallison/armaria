aadf43c286da61779b09e96e5667b3895bb6dddd
Thiago Tallison <thiagotallison@gmail.com>
2023-09-04 19:39:31 -0300

    refactor: mudar metodo http para deletar um equipamento

--- src/main/java/com/example/armaria/controllers/equipamento/DeletarEquipamentoController.java
+++ src/main/java/com/example/armaria/controllers/equipamento/DeletarEquipamentoController.java
@@ -2,8 +2,8 @@ package com.example.armaria.controllers.equipamento;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +19,7 @@ public class DeletarEquipamentoController {
     this.deletarEquipamentoUseCase = deletarEquipamentoUseCase;
   }
 
-  @PutMapping("{numSerie}")
+  @DeleteMapping("{numSerie}")
   public ResponseEntity<Void> handle(@PathVariable String numSerie) {
     deletarEquipamentoUseCase.execute(numSerie);
 
