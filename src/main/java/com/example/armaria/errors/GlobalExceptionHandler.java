0f3ae208b8ce391fe90ff1366530348321778c28
Thiago Tallison <thiagotallison@gmail.com>
2023-09-05 10:38:56 -0300

    feat: validar requisicao de criacao de equipamento

--- src/main/java/com/example/armaria/errors/GlobalExceptionHandler.java
+++ src/main/java/com/example/armaria/errors/GlobalExceptionHandler.java
@@ -1,7 +1,14 @@
 package com.example.armaria.errors;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -31,6 +38,22 @@ public class GlobalExceptionHandler {
   public ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex) {
     String errorMessage = "Required request parameter '" + ex.getParameterName() + "' is not present";
     return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException e) {
+    Map<String, List<String>> body = new HashMap<>();
+
+    List<String> errors = e.getBindingResult()
+        .getFieldErrors()
+        .stream()
+        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+        .collect(Collectors.toList());
+
+    body.put("errors", errors);
+
+    return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
   }
 
 }
