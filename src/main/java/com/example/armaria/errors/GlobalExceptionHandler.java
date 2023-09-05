5e917bb2cf2a272020488127b68d5d296357f5f1
Thiago Tallison <thiagotallison@gmail.com>
2023-09-05 11:57:05 -0300

    feat: tratamento de erro global TamanhoDaPaginaNegativoException.class

--- src/main/java/com/example/armaria/errors/GlobalExceptionHandler.java
+++ src/main/java/com/example/armaria/errors/GlobalExceptionHandler.java
@@ -69,6 +69,13 @@ public class GlobalExceptionHandler {
         .body(new MensagemDeErroGeral(e.getMessage()));
   }
 
+  @ExceptionHandler(TamanhoDaPaginaNegativoException.class)
+  public ResponseEntity<MensagemDeErroGeral> handlePaginaNaoExistenteException(
+      TamanhoDaPaginaNegativoException e) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(new MensagemDeErroGeral(e.getMessage()));
+  }
+
   @ExceptionHandler(MissingServletRequestParameterException.class)
   public ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex) {
     String errorMessage = "Required request parameter '" + ex.getParameterName() + "' is not present";
