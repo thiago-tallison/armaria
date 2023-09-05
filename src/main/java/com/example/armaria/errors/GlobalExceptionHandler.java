18d7626e776ad066cecb3ffc27be00613befb3e3
Thiago Tallison <thiagotallison@gmail.com>
2023-09-05 11:56:30 -0300

    feat: tratamento de erro global PaginaNaoExistenteException.class

--- src/main/java/com/example/armaria/errors/GlobalExceptionHandler.java
+++ src/main/java/com/example/armaria/errors/GlobalExceptionHandler.java
@@ -62,6 +62,13 @@ public class GlobalExceptionHandler {
         .body(new MensagemDeErroGeral(e.getMessage()));
   }
 
+  @ExceptionHandler(PaginaNaoExistenteException.class)
+  public ResponseEntity<MensagemDeErroGeral> handlePaginaNaoExistenteException(
+      PaginaNaoExistenteException e) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(new MensagemDeErroGeral(e.getMessage()));
+  }
+
   @ExceptionHandler(MissingServletRequestParameterException.class)
   public ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex) {
     String errorMessage = "Required request parameter '" + ex.getParameterName() + "' is not present";
