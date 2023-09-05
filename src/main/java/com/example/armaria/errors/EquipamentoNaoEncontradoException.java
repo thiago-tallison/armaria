2fbdc5c4c7eac8195e9f799bfe9c2ac6b680c68e
Thiago Tallison <thiagotallison@gmail.com>
2023-09-05 15:39:42 -0300

    feat: buscar ItemEstoque por numSerie

--- src/main/java/com/example/armaria/errors/EquipamentoNaoEncontradoException.java
+++ src/main/java/com/example/armaria/errors/EquipamentoNaoEncontradoException.java
@@ -4,4 +4,8 @@ public class EquipamentoNaoEncontradoException extends RuntimeException {
   public EquipamentoNaoEncontradoException() {
     super("Equipamento não encontrado");
   }
+
+  public EquipamentoNaoEncontradoException(String numSerie) {
+    super("Equipamento não encontrado: " + numSerie);
+  }
 }
